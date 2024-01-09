b51b216d329bc1f3114ed67820751a17d827232e
Andreugc19 <agarcia20507@iesjoanramis.org>
2024-01-09 16:55:17 +0100

    React Andreu Garcia

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Navbar from './Navbar';
 import Inicio from './Inicio';
 
--- src/Inicio.js
+++ src/Inicio.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 const Inicio = () => {
+
+    const [blogs, setBlogs] = useState([
+        { titulo: "Mi nueva web", body: "texto para mostrar...", autor: "Andreu Garcia", id: 1 },
+        { titulo: "Futbol Migjorn", body: "mucho futbol", autor: "Biel Medina", id: 2 },
+        { titulo: "Elementos Musicales", body: "fundamentos de la musica", autor: "Tomas Rossello", id: 3 }
+    ])
+
     return ( 
         <div className="home">
-            <h2>Pagina de Inicio</h2>
+            { blogs.map((blog)=>{
+                return(
+                <div className="blog-preview" key={ blog.id }>
+                    <h2>{ blog.titulo }</h2>
+                    <p>Escrito por { blog.autor }</p>
+                </div>)
+            })}
         </div>
      );
 }
